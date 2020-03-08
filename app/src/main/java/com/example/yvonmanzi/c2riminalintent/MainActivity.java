21c3a30f11ce5876b6a83e2f183ac6ef63bde37f
Yvon <ym2637@columbia.edu>
2020-03-08 17:36:51 +0300

    CrimeFragment is now displayed on the screen! Fragment Manager has added to the transaction.

--- app/src/main/java/com/example/yvonmanzi/c2riminalintent/MainActivity.java
+++ app/src/main/java/com/example/yvonmanzi/c2riminalintent/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.yvonmanzi.c2riminalintent;
 
 
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,5 +15,20 @@ public class MainActivity extends AppCompatActivity {
 
         /* this manager does hold fragment list and back stack*/
         FragmentManager fm = getSupportFragmentManager();
+
+
+        /*check from 'fm' which is FManager if it has a fragment identified by the container id.
+        * But why would it have it? It is possible that this activity might have been destroyed(bse, for example,
+        * of rotation) after the fragment was added on the list already. When this activity dies, the list is kept, so you havee
+        * to check if the fragment is not already in the list before you can create a new instance of it.
+        * */
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        //fragment transactions help us add, remove, attach, detach, or replace fragments
+        //from a fragment list
+        if(fragment == null){
+            fragment = new CrimeFragment();
+            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
+        }
     }
 }
